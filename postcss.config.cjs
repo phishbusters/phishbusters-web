9fc45000523108813851b62cfffb24829fcb8876
Fernando Mirabile <fpmirabile@gmail.com>
2023-08-28 17:31:01 -0300

    Adding config files

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const postCssImport = require('postcss-import');
+const advancedVariables = require('postcss-advanced-variables');
+const nesting = require('postcss-nesting');
 
 module.exports = {
-	plugins: [tailwindcss(), autoprefixer()]
+	plugins: [tailwindcss(), autoprefixer(), postCssImport(), advancedVariables(), nesting()]
 };
